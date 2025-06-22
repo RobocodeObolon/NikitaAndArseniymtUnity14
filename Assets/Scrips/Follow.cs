a10f8ef68669c1fa368d5428a6c5ff7f33cff05f
RobocodeObolon <nstargamess@gmail.com>
2025-06-22 15:59:18 +0300

    GOVNO TUPOE

--- Assets/Scrips/Follow.cs
+++ Assets/Scrips/Follow.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Follow : MonoBehaviour
 {
+    public Patrol patrol;
+    public AIDestinationSetter follow;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(transform.position, player.position) > 3)
+        {
+            patrol.enabled = true;
+            follow.enabled = false;
+        }
+        else
+        {
+            patrol.enabled = false;
+            follow.enabled = true;
+        }
     }
 }
